101d608d4604de9095efdddea451394b801cf94e
ghulemahesh2509-ops <ghulemahesh2509@gmail.com>
2026-08-26 15:38:42 +0530

    Update style.css

---
+++ style.css
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const links = document.querySelectorAll("a[href^='#']");
+
+  links.forEach(link => {
+    link.addEventListener("click", function(e) {
+      const target = document.querySelector(this.getAttribute("href"));
+
+      if (target) {
+        e.preventDefault();
+
+        target.scrollIntoView({
+          behavior: "smooth"
+        });
+      }
+    });
+  });
+
+  console.log("Mahesh Portfolio Loaded Successfully 🚀");
+
+});
